17c2335310b13279a7f10a40479ed484ba3166da
sischi <simon-schiller@gmx.de>
2021-02-13 21:23:37 +0100

    renamed base controller

--- src/main/java/com/sischi/selvebridge/rest/SelveRestController.java
+++ src/main/java/com/sischi/selvebridge/rest/SelveRestController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-@RequestMapping("main")
+@RequestMapping("/app")
 public class SelveRestController extends AbstractSelveRestController {
 
     @Value("${app.version}")
