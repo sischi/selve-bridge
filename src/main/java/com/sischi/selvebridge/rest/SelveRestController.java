0eb7f374ad8453606ee92953b90af9f92185180f
sischi <simon-schiller@gmx.de>
2021-02-28 21:03:11 +0100

    added rest endpoint for decoding base64 mask

--- src/main/java/com/sischi/selvebridge/rest/SelveRestController.java
+++ src/main/java/com/sischi/selvebridge/rest/SelveRestController.java
@@ -2,6 +2,7 @@ package com.sischi.selvebridge.rest;
 
 import com.sischi.selvebridge.core.entities.message.SelveXmlMethodCall;
 import com.sischi.selvebridge.core.gateway.Conversation;
+import com.sischi.selvebridge.util.Utils;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -31,6 +33,11 @@ public class SelveRestController extends AbstractSelveRestController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "something went wrong!");
         }
         return conversation;
+    }
+
+    @GetMapping("/decodeBase64")
+    public ResponseEntity<String> decodeBase64(@RequestParam String base64) {
+        return new ResponseEntity<String>("base64: '"+ base64 +"'  --->   binary: '"+ Utils.base64ToBinary(base64) +"'", HttpStatus.OK);
     }
 
     
