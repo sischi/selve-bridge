c792b2c7d4f657c0b73015155775001d755ccdfb
sischi <simon-schiller@gmx.de>
2021-03-02 20:01:39 +0100

    extracted method names to enum

--- src/main/java/com/sischi/selvebridge/gateway/models/MessageFactory.java
+++ src/main/java/com/sischi/selvebridge/gateway/models/MessageFactory.java
@@ -1,5 +1,6 @@
 package com.sischi.selvebridge.gateway.models;
 
+import com.sischi.selvebridge.gateway.models.enums.MethodNames;
 import com.sischi.selvebridge.gateway.models.message.SelveXmlMessage;
 import com.sischi.selvebridge.gateway.models.message.SelveXmlMethodCall;
 import com.sischi.selvebridge.gateway.models.message.SelveXmlMethodParameter;
@@ -11,23 +12,31 @@ public class MessageFactory {
     public static class Service {
         public static SelveXmlMessage ping() {
             return new SelveXmlMethodCall()
-                .withMethodName("selve.GW.service.ping");
+                .withMethodName(MethodNames.Service.PING);
         }
         public static SelveXmlMessage getState() {
             return new SelveXmlMethodCall()
-                .withMethodName("selve.GW.service.getState");
+                .withMethodName(MethodNames.Service.GET_STATE);
         }
         public static SelveXmlMessage getVersion() {
             return new SelveXmlMethodCall()
-                .withMethodName("selve.GW.service.getVersion");
+                .withMethodName(MethodNames.Service.GET_VERSION);
         }
+        public static SelveXmlMessage reset() {
+			return new SelveXmlMethodCall()
+                .withMethodName(MethodNames.Service.RESET);
+		}
+		public static SelveXmlMessage factoryReset() {
+			return new SelveXmlMethodCall()
+                .withMethodName(MethodNames.Service.FACTORY_RESET);
+		}
         public static SelveXmlMessage getLed() {
             return new SelveXmlMethodCall()
-                .withMethodName("selve.GW.service.getLED");
+                .withMethodName(MethodNames.Service.GET_LED);
         }
         public static SelveXmlMessage setLed(int ledMode) {
             return new SelveXmlMethodCall()
-                .withMethodName("selve.GW.service.setLED")
+                .withMethodName(MethodNames.Service.SET_LED)
                 .withParameter(
                     new SelveXmlMethodParameter(ParameterType.INT, ledMode)
                 );
@@ -38,20 +47,20 @@ public class MessageFactory {
     public static class Device {
         public static SelveXmlMessage scanStart() {
             return new SelveXmlMethodCall()
-                .withMethodName("selve.GW.device.scanStart");
+                .withMethodName(MethodNames.Device.SCAN_START);
         }
         public static SelveXmlMessage scanStop() {
             return new SelveXmlMethodCall()
-                .withMethodName("selve.GW.device.scanStop");
+                .withMethodName(MethodNames.Device.SCAN_STOP);
         }
         public static SelveXmlMessage scanResult() {
             return new SelveXmlMethodCall()
-                .withMethodName("selve.GW.device.scanResult");
+                .withMethodName(MethodNames.Device.SCAN_RESULT);
         }
         public static SelveXmlMessage getInfo(int deviceId) {
             Validator.validateCommeoDeviceId(deviceId);
             return new SelveXmlMethodCall()
-                .withMethodName("selve.GW.device.getInfo")
+                .withMethodName(MethodNames.Device.GET_INFO)
                 .withParameter(
                     new SelveXmlMethodParameter(ParameterType.INT, deviceId)
                 );
@@ -59,7 +68,7 @@ public class MessageFactory {
         public static SelveXmlMessage save(int deviceId) {
             Validator.validateCommeoDeviceId(deviceId);
             return new SelveXmlMethodCall()
-                .withMethodName("selve.GW.device.save")
+                .withMethodName(MethodNames.Device.SAVE)
                 .withParameter(
                     new SelveXmlMethodParameter(ParameterType.INT, deviceId)
                 );
@@ -67,19 +76,19 @@ public class MessageFactory {
         public static SelveXmlMessage getValues(int deviceId) {
             Validator.validateCommeoDeviceId(deviceId);
             return new SelveXmlMethodCall()
-                .withMethodName("selve.GW.device.getValues")
+                .withMethodName(MethodNames.Device.GET_VALUES)
                 .withParameter(
                     new SelveXmlMethodParameter(ParameterType.INT, deviceId)
                 );
         }
         public static SelveXmlMessage getIds() {
             return new SelveXmlMethodCall()
-                .withMethodName("selve.GW.device.getIDs");
+                .withMethodName(MethodNames.Device.GET_IDS);
         }
         public static SelveXmlMessage setFunction(int deviceId, int function) {
             Validator.validateCommeoDeviceId(deviceId);
             return new SelveXmlMethodCall()
-                .withMethodName("selve.GW.device.setFunction")
+                .withMethodName(MethodNames.Device.SET_FUNCTION)
                 .withParameter(
                     new SelveXmlMethodParameter(ParameterType.INT, deviceId),
                     new SelveXmlMethodParameter(ParameterType.INT, function)
@@ -88,7 +97,7 @@ public class MessageFactory {
         public static SelveXmlMessage setLabel(int deviceId, String label) {
             Validator.validateCommeoDeviceId(deviceId);
             return new SelveXmlMethodCall()
-                .withMethodName("selve.GW.device.setLabel")
+                .withMethodName(MethodNames.Device.SET_LABEL)
                 .withParameter(
                     new SelveXmlMethodParameter(ParameterType.INT, deviceId),
                     new SelveXmlMethodParameter(ParameterType.STRING, label)
@@ -97,7 +106,7 @@ public class MessageFactory {
         public static SelveXmlMessage setType(int deviceId, int type) {
             Validator.validateCommeoDeviceId(deviceId);
             return new SelveXmlMethodCall()
-                .withMethodName("selve.GW.device.setType")
+                .withMethodName(MethodNames.Device.SET_TYPE)
                 .withParameter(
                     new SelveXmlMethodParameter(ParameterType.INT, deviceId),
                     new SelveXmlMethodParameter(ParameterType.INT, type)
@@ -106,7 +115,7 @@ public class MessageFactory {
         public static SelveXmlMessage delete(int deviceId) {
             Validator.validateCommeoDeviceId(deviceId);
             return new SelveXmlMethodCall()
-                .withMethodName("selve.GW.device.delete")
+                .withMethodName(MethodNames.Device.DELETE)
                 .withParameter(
                     new SelveXmlMethodParameter(ParameterType.INT, deviceId)
                 );
@@ -117,7 +126,7 @@ public class MessageFactory {
         public static SelveXmlMessage device(int deviceId, int command, int type, int parameter) {
             Validator.validateCommeoDeviceId(deviceId);
             return new SelveXmlMethodCall()
-                .withMethodName("selve.GW.command.device")
+                .withMethodName(MethodNames.Command.DEVICE)
                 .withParameter(
                     new SelveXmlMethodParameter(ParameterType.INT, deviceId),
                     new SelveXmlMethodParameter(ParameterType.INT, command),
@@ -128,7 +137,7 @@ public class MessageFactory {
         public static SelveXmlMessage group(int groupId, int command, int type, int parameter) {
             Validator.validateCommeoGroupId(groupId);
             return new SelveXmlMethodCall()
-                .withMethodName("selve.GW.command.group")
+                .withMethodName(MethodNames.Command.GROUP)
                 .withParameter(
                     new SelveXmlMethodParameter(ParameterType.INT, groupId),
                     new SelveXmlMethodParameter(ParameterType.INT, command),
--- src/main/java/com/sischi/selvebridge/gateway/models/enums/MethodNames.java
+++ src/main/java/com/sischi/selvebridge/gateway/models/enums/MethodNames.java
@@ -1,12 +1,40 @@
 package com.sischi.selvebridge.gateway.models.enums;
 
 public class MethodNames {
+
+    public static class Service {
+        public static final String PING = "selve.GW.service.ping";
+        public static final String GET_STATE = "selve.GW.service.getState";
+        public static final String GET_VERSION = "selve.GW.service.getVersion";
+        public static final String RESET = "selve.GW.service.reset";
+        public static final String FACTORY_RESET = "selve.GW.service.factoryReset";
+        public static final String GET_LED = "selve.GW.service.getLED";
+        public static final String SET_LED = "selve.GW.service.setLED";
+    }
     
-    public static final String DEVICE_SCANRESULT = "selve.GW.device.scanResult";
-    public static final String DEVICE_GETINFO = "selve.GW.device.getInfo";
+    public static class Device {
+        public static final String SCAN_START = "selve.GW.device.scanStart";
+        public static final String SCAN_STOP = "selve.GW.device.scanStop";
+        public static final String SCAN_RESULT = "selve.GW.device.scanResult";
+        public static final String SAVE = "selve.GW.device.save";
+        public static final String GET_IDS = "selve.GW.device.getIDs";
+        public static final String GET_INFO = "selve.GW.device.getInfo";
+        public static final String GET_VALUES = "selve.GW.device.getValues";
+        public static final String SET_FUNCTION = "selve.GW.device.setFunction";
+        public static final String SET_LABEL = "selve.GW.device.setLabel";
+        public static final String SET_TYPE = "selve.GW.device.setType";
+        public static final String DELETE = "selve.GW.device.delete";
+        public static final String WRITE_MANUAL = "selve.GW.device.writeManual";
+    }
 
-    public static final String COMMAND_RESULT = "selve.GW.command.result";
+    public static class Command {
+        public static final String DEVICE = "selve.GW.command.device";
+        public static final String GROUP = "selve.GW.command.group";
+        public static final String RESULT = "selve.GW.command.result";
+    }
 
-    public static final String EVENT_DEVICE = "selve.GW.event.device";
-    public static final String EVENT_LOG = "selve.GW.event.log";
+    public static class Event {
+        public static final String DEVICE = "selve.GW.event.device";
+        public static final String LOG = "selve.GW.event.log";
+    }
 }
